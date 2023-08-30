6faf771e99f191e6fd3e6876dca7dd9f71c1cb46
Daniel Whitten <danieljwhitten@gmail.com>
2023-08-30 20:45:06 -0300

    Added master functionto import and label in one call

--- data_from_txt_and_sas.R
+++ data_from_txt_and_sas.R
@@ -17,34 +17,34 @@ read_txt <- function(txt_data,
 #' This is typically in the Data_Donnees/Syntax_Syntaxe/SAS directory, named 
 #' ENGLYYYYFRAN_X_i.sas
 #'
-#' @return A tibble
+#' @return An object of class "Tibble"
 #'
 #' @examples
 #' ctns_pumf <- read_txt("SAS/Data_Donnees/CTNS2021ECTN_P.txt",
-#'                       "SAS/Data_Donnees/SyntaxSyntax_Syntaxe/SAS/CTNS2021ECTN_P_i.SAS")
-#' 
-  print("Loading input specifications...")
+#'                       "SAS/Data_Donnees/Syntax_Syntaxe/SAS/CTNS2021ECTN_P_i.SAS")
+
+  # read in input mapping as table
   data_map <- read_table(syntax_i,
                          col_names = FALSE,
                          skip = 2,
                          progress = FALSE,
                          show_col_types = FALSE,
                          skip_empty_rows = TRUE) %>% 
-    filter(X1 == "@") %>% 
-    mutate(is_string = X4 == "$",
+    filter(X1 == "@") %>% # limit to mapping rows
+    mutate(is_string = X4 == "$", # find strings
            X4 = floor(as.numeric(ifelse(X4 == "$",
                                         X5,
-                                        X4)))) %>% 
+                                        X4)))) %>%  # re-align offset string rows
     select(X3, X4, is_string)
   
-  print("Reading in data from file...")
+  # read in the text data based on position and assign name
   dat <- read_fwf(txt_data,
                   fwf_widths(data_map$X4,
                              data_map$X3),
                   show_col_types = FALSE,
                   progress = FALSE) %>% 
     mutate(across(all_of(data_map$X3[!data_map$is_string]),
-                  as.numeric))
+                  as.numeric)) # make non strings explicitly numeric
   
   return(dat)
 }
@@ -66,10 +66,11 @@ apply_labels <- function(dat,
 #'
 #' @examples
 #' ctns_pumf <- read_txt("SAS/Data_Donnees/CTNS2021ECTN_P.txt",
-#'                       "SAS/Data_Donnees/SyntaxSyntax_Syntaxe/SAS/CTNS2021ECTN_P_i.SAS")
+#'                       "SAS/Data_Donnees/Syntax_Syntaxe/SAS/CTNS2021ECTN_P_i.SAS")
 #' ctns_pumf <- apply_labels(ctns_pumf,
-#'                           "SAS/Data_Donnees/SyntaxSyntax_Syntaxe/SAS/CTNS2021ECTN_P_lbe.SAS")
+#'                           "SAS/Data_Donnees/Syntax_Syntaxe/SAS/CTNS2021ECTN_P_lbe.SAS")
   
+  # read in syntax mapping as table
   label_map <-
     read_delim(
       syntax_lb,
@@ -80,14 +81,14 @@ apply_labels <- function(dat,
       skip = 1,
       locale = locale(encoding = "latin1")
     )
-  label_map <- label_map[1:nrow(label_map)-1,]
+  label_map <- label_map[1:nrow(label_map)-1,] #drop last row
   
   label_map_list <- setNames(as.list(label_map$X2), 
-                             label_map$X1)
+                             label_map$X1) # collapse into named list
   
-  var_label(dat) <- label_map_list
+  var_label(dat) <- label_map_list # apply
   
-  dat <- as_tibble(dat)
+  dat <- as_tibble(dat) # just in case read_txt wasn't used
   
   return(dat)
 }
@@ -116,12 +117,12 @@ apply_formats <- function(dat,
 #'
 #' @examples
 #' ctns_pumf <- read_txt("SAS/Data_Donnees/CTNS2021ECTN_P.txt",
-#'                       "SAS/Data_Donnees/SyntaxSyntax_Syntaxe/SAS/CTNS2021ECTN_P_i.SAS")
+#'                       "SAS/Data_Donnees/Syntax_Syntaxe/SAS/CTNS2021ECTN_P_i.SAS")
 #' ctns_pumf <- apply_labels(ctns_pumf,
-#'                           "SAS/Data_Donnees/SyntaxSyntax_Syntaxe/SAS/CTNS2021ECTN_P_lbe.SAS")
+#'                           "SAS/Data_Donnees/Syntax_Syntaxe/SAS/CTNS2021ECTN_P_lbe.SAS")
 #' ctns_pumf <- apply_formats(ctns_pumf,
-#'                            "~/data/data_sources/CTNS2021/SAS/Data_Donnees/Syntax_Syntaxe/SAS/CTNS2021ECTN_P_pfe.SAS",
-#'                            "~/data/data_sources/CTNS2021/SAS/Data_Donnees/Syntax_Syntaxe/SAS/CTNS2021ECTN_P_fmt.SAS")
+#'                            "SAS/Data_Donnees/Syntax_Syntaxe/SAS/CTNS2021ECTN_P_pfe.SAS",
+#'                            "SAS/Data_Donnees/Syntax_Syntaxe/SAS/CTNS2021ECTN_P_fmt.SAS")
   
   # Stream in raw proc freq data as 1-col tibble for string functions
   con <-
@@ -177,5 +178,74 @@ apply_formats <- function(dat,
   
   dat = exec(set_value_labels, !!!format_map_list) # pass list as args
   return(dat)
+}
+
+
+load_statcan_data <- function(txt_data,
+                              syntax_i,
+                              syntax_lb,
+                              syntax_pf,
+                              syntax_fmt,
+                              bsw_data = NULL,
+                              syntax_bsw_i = NULL){
+#' Import Statistics Canada Dataset
+#' 
+#' Import a StatCan dataset using a .txt data file and .sas layout syntax files.
+#' Optionally, also specify a bootstrap replicate weight (bsw) .txt data file
+#' and corresponding .sas input layout syntax file, and these will be joined to
+#' the final dataset.
+#' 
+#' Output data will have variable labels and value labels (formats) applied per
+#' the SAS specifications. These will use the haven_labelled class in order to
+#' match the output when importing a .sas7bdat dataset via haven::read_sas()
+#'
+#' @param txt_data An object of class "String". Path to the .txt data file.
+#' This is typically in the Data_Donnees directory, named ENGLYYYYFRAN_X.txt
+#' @param syntax_i An object of class "String". Path to the .sas input syntax.
+#' This is typically in the Data_Donnees/Syntax_Syntaxe/SAS directory, named 
+#' ENGLYYYYFRAN_X_i.sas
+#' @param syntax_lb An object of Class "String". Path to the .sas label syntax
+#' in either English or French. This is typically in the 
+#' Data_Donnees/Syntax_Syntaxe/SAS directory, named ENGLYYYYFRAN_X_lbx.sas
+#' @param syntax_pf An object of Class "String". Path to the .sas format
+#' definition (proc format) syntax in either English or French. This is typically 
+#' in the Data_Donnees/Syntax_Syntaxe/SAS directory, named ENGLYYYYFRAN_X_pfx.sas
+#' @param syntax_fmt An object of Class "String". Path to the .sas format
+#' assignment (format) syntax. This is typically in the 
+#' Data_Donnees/Syntax_Syntaxe/SAS directory, named ENGLYYYYFRAN_X_fmt.sas
+#' @param bsw_data An object of class "String". Path to the BSW .txt data file.
+#' This is typically in the Data_Donnees directory, named ENGLYYYYFRAN_X_bsw.txt
+#' @param syntax_bsw_i An object of class "String". Path to the .sas input 
+#' syntax for the BSW file. This is typically in the Data_Donnees/Syntax_Syntaxe/SAS  
+#' directory, named ENGLYYYYFRAN_X_bsw_i.sas
+#'
+#' @return An object of class "Tibble"
+#'
+  #' @examples
+  #' ctns_pumf <- 
+  #'   load_statcan_data("SAS/Data_Donnees/CTNS2021ECTN_P.txt",
+  #'                     "SAS/Data_Donnees/Syntax_Syntaxe/SAS/CTNS2021ECTN_P_i.SAS",
+  #'                     "SAS/Data_Donnees/Syntax_Syntaxe/SAS/CTNS2021ECTN_P_lbe.SAS",
+  #'                     "SAS/Data_Donnees/Syntax_Syntaxe/SAS/CTNS2021ECTN_P_pfe.SAS",
+  #'                     "SAS/Data_Donnees/Syntax_Syntaxe/SAS/CTNS2021ECTN_P_fmt.SAS")  
+  
+  print("Loading Data...")
+  
+  out <- read_txt(txt_data, syntax_i) %>% 
+    apply_labels(syntax_lb) %>% 
+    apply_formats(syntax_pf, syntax_fmt)
+  
+  # if all bsw args are there, build and join bsw data
+  if (!any(sapply(list(bsw_data, syntax_bsw_i), is.null))) {
+    
+    print("Loading bootstrap replicate weights...")
+    
+    bsw <- read_txt(bsw_data, syntax_bsw_i)
+    
+    out <- out %>% inner_join(bsw)
+  } else print("Bootstrap replicate weight info not provided")
+  
+  return(out)
+  
 }
 
